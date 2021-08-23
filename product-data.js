94771ac7abfe6dfc60776a8539bc36938f16e744
Dylan Floyd <floyd800@gmail.com>
2021-08-23 15:05:59 -0700

    working product page rendering

---
+++ product-data.js
@@ -0,0 +1,18 @@
+export const products = [
+    {
+        id: 123,
+        name: 'Tony the Tiger',
+        image: './assets/tony-the-tiger.jpg',
+        description: 'Tony the Tiger is looking for a loving home after the bankruptcy of Nabisco International. Good with kids, other cats, and dogs. Mildy in the uncanny valley. All IP included.',
+        category: 'Corporate Shill Cat',
+        price: '$400m'
+    },
+    {
+        id: 124,
+        name: 'Stella',
+        image: './assets/stella.jpg',
+        description: 'The best cat.',
+        category: 'Domestic Long Hair',
+        price: 'Not for sale'
+    }
+];
---
+++ test/render-utils.test.js
@@ -0,0 +1,31 @@
+// IMPORT MODULES under test here:
+import { renderProduct } from '../render-utils.js';
+import { products } from '../product-data.js';
+
+const test = QUnit.test;
+
+test('time to test a function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected =
+        // eslint-disable-next-line no-useless-escape
+        '<li id=\"123\" class="cat">' +
+            '<img src="./assets/tony-the-tiger.jpg">' +
+            '<div class="col">' +
+                '<div class="row">' +
+                    '<h3>Tony the Tiger</h3>' +
+                    '<span class="price-span">Price: $400m</span>' +
+                '</div>' +
+                '<p>Tony the Tiger is looking for a loving home after the bankruptcy of Nabisco International. Good with kids, other cats, and dogs. Mildy in the uncanny valley. All IP included.</p>' +
+                '<span>Category: Corporate Shill Cat</span>' +
+            '</div>' +
+        '</li>';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderProduct(products[0]).outerHTML;
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
