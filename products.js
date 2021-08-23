c0090c6ed2687f9f4df1dc48b4c8e2cb63ab6ef8
Dylan Floyd <floyd800@gmail.com>
2021-08-23 14:26:39 -0700

    initial single-product design

---
+++ products.js
@@ -0,0 +1,10 @@
+export const products = [
+    {
+        id: 123,
+        name: 'Tony the Tiger',
+        image: './assets/tony-the-tiger.jpg',
+        description: 'Tony the Tiger is looking for a loving home after the bankruptcy of Nabisco International. Good with kids, other cats, and dogs. Mildy in the uncanny valley. All IP included.',
+        category: 'Corporate Shill Cat',
+        price: '400m'
+    }
+];
