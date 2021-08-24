065cd36e808aa6ebc00078f0d7a4f056a5e2be08
Dylan Floyd <floyd800@gmail.com>
2021-08-24 14:30:26 -0700

    add cart-data.js

---
+++ data/cart-data.js
@@ -0,0 +1,14 @@
+export const cartData = [
+    {
+        id: 126,
+        quantity: 2
+    },
+    {
+        id: 127,
+        quantity: 5
+    },
+    {
+        id: 125,
+        quantity: 32
+    }
+];
