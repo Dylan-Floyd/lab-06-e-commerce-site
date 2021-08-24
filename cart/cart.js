a94677347f395cab9584a40f7c694f6d429c8378
Dylan Floyd <floyd800@gmail.com>
2021-08-24 15:44:45 -0700

    add grand total to cart page

--- cart/cart.js
+++ cart/cart.js
@@ -1,12 +1,15 @@
 import { products } from '../data/product-data.js';
 import { cartData } from '../data/cart-data.js';
 import { renderLineItem } from './render-line-item.js';
-import { findById } from '../utils.js';
+import { calcOrderTotal, findById } from '../utils.js';
 
 const tableBody = document.querySelector('#table-body');
+const grandTotal = document.querySelector('#grand-total');
 
 for (let cartItem of cartData) {
     const productData = findById(products, cartItem.id);
     const newRow = renderLineItem(cartItem, productData);
     tableBody.appendChild(newRow);
 }
+
+grandTotal.textContent = calcOrderTotal(cartData, products);
