0e17ed62d07258c777673fbd198bfc4dfe837e20
Dylan Floyd <floyd800@gmail.com>
2021-08-25 16:29:26 -0700

    updated cart page to use local storage, implemented place order button

--- cart/cart.js
+++ cart/cart.js
@@ -1,9 +1,36 @@
 import { cats, fruits } from '../data/product-data.js';
-import { catCart, fruitCart } from '../data/cart-data.js';
+import { CAT_CART, FRUIT_CART, getCart, updateCart } from '../data/cart-utils.js';
 import { renderTable } from './cart-render-utils.js';
+import { calcMultiOrderTotal } from '../utils.js';
 
 const tableContainer = document.querySelector('#table-container');
+const grandestTotalSpan = document.querySelector('#grandest-total-span');
+const orderButton = document.querySelector('#order-button');
+
+let catCart = getCart(CAT_CART);
+let fruitCart = getCart(FRUIT_CART);
 
 const catTable = renderTable(catCart, cats, 'Cat');
 const fruitTable = renderTable(fruitCart, fruits, 'Fruit');
 tableContainer.append(catTable, fruitTable);
+
+const allOrderData = [
+    {
+        cartData: catCart,
+        products: cats
+    },
+    {
+        cartData: fruitCart,
+        products: fruits
+    }
+];
+
+let grandestTotalString = calcMultiOrderTotal(allOrderData);
+grandestTotalSpan.textContent = 'Grandest Total: ' + grandestTotalString;
+
+orderButton.addEventListener('click', () => {
+    let stringyData = JSON.stringify(catCart) + '\n' + JSON.stringify(fruitCart);
+    alert('You bought stuff! \n' + stringyData);
+    updateCart(CAT_CART, []);
+    updateCart(FRUIT_CART, []);
+});
